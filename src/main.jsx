f6ffb98d9487fcdf256c0ac010b564d168bc5b93
NickArtCorp <ncorp237@gmail.com>
2025-10-07 07:25:50 +0100

    hey

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
+import { AppContextProvider } from "./AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 
 root.render(
-
-    <App />
+   <HashRouter>
+   <AppContextProvider>
+      <App />
+    </AppContextProvider>
+    </HashRouter>
+  
  ,
 );
