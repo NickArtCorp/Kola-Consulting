aede827709956194230cc4c407e66e10df0bb92b
NickArtCorp <ncorp237@gmail.com>
2025-10-07 19:33:53 +0100

    hey

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/Kola_Consulting/',
+  base: process.env.VITE_BASE_PATH || '/Kola-Consulting/',
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
